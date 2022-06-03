86331a01966311063514541cbe646be28dcc481b
Longnn <longnn17.developer@gmail.com>
2022-06-03 19:57:35 +0700

    fix cookie

--- src/app.ts
+++ src/app.ts
@@ -28,7 +28,7 @@ export default class App {
   constructor() {
     this.app = express();
     this.port = process.env.PORT || 4000;
-    this.next = 'https://graph.facebook.com/v13.0/pages/search?access_token=EAABsbCS1iHgBAGNU5rxcsa50gO6NvvGZCFMZBqevpuqrddfy3ZBTh2iy9PL7im5sU8dsBc0CZCHYz2Fp89znzvY7116IS7A2p8wcZCEQZBE1K92T6gwZCB1MTCFZBsLXw3ehicssZBbRZCyM6r8ZBblWBZA21GZCGEE1i9qagWfZAHemmqeR0qX76h7mSqe4FA1dHymwgZD&__cppo=1&debug=all&format=json&limit=300&method=get&pretty=0&q=nails&state=%22united%20state%22&suppress_http_code=1&transport=cors&type=country'
+    this.next = 'https://graph.facebook.com/v13.0/pages/search?access_token=EAABsbCS1iHgBABeqwRb0CDiDAdB1PUHqyZCVRcuHGbP0S27AqFnqRyDZCh2K0J3ZAgvAvkllW7OI1ZCxi0SaIG4meZAexubzJ4aIgB2rlRao2lvZCNhmYB5KwZBQzw1D79LqiCKHLe6rAZCWuGX6D3cgCAgi7eIt2oTmjw8woVwpdYG4yh7CXR8MFkrSiegTHX4ZD&__cppo=1&debug=all&format=json&method=get&pretty=0&q=nails&state=%22united%20state%22&suppress_http_code=1&transport=cors&type=country'
   }
 
 
@@ -74,7 +74,7 @@ export default class App {
         'Accept': '*/*',
         'Content-Type': 'application/x-www-form-urlencoded',
         'Origin': 'https://developers.facebook.com',
-        'Cookie': 'wd=1650x404; usida=eyJ2ZXIiOjEsImlkIjoiQXJjOXYzaXV0MmtzeSIsInRpbWUiOjE2NTMyMDQ1NDZ9; cppo=1; fr=0RTEYl2K91g3DU27Q.AWUML3Ep0mitrHrAgdokduamOz0.Biidm_.ZU.AAA.0.0.Biid3n.AWVRGGhohcM; c_user=100066192924982; xs=5%3Aymwyn3MxZYEPLQ%3A2%3A1653055477%3A-1%3A14128%3A%3AAcXyiBJbeXDJvaLfOxMJnvjqoTixIn3zzLg-BP4bBg; presence=C%7B%22t3%22%3A%5B%5D%2C%22utc3%22%3A1653156441657%2C%22v%22%3A1%7D; sb=fRZdYmmkyLCqUiLG_yqHqUa2; locale=en_US; dpr=2; datr=fRZdYvFPptvdypB4vUSP-Iy_',
+        'Cookie': 'usida=eyJ2ZXIiOjEsImlkIjoiQXJjd2pncjFubzk0ZW0iLCJ0aW1lIjoxNjU0MjYwOTA2fQ%3D%3D; wd=1650x404; cppo=1; c_user=100066192924982; fr=0XFKI17wpVlV75XcS.AWU5tBMU1xV4g9f_wrwDZwHoEYY.BimgLJ.ZU.AAA.0.0.BimgLJ.AWVPPRruYRc; xs=38%3AbwcltOQYQGvyxA%3A2%3A1654260359%3A-1%3A14128%3A%3AAcUv80i1oWm0RJIa2Im0qQhhxRhcgC_rkglqQqFpdA; sb=fRZdYmmkyLCqUiLG_yqHqUa2; dpr=2; datr=fRZdYvFPptvdypB4vUSP-Iy_',
         'Host': 'graph.facebook.com',
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Safari/605.1.15',
         'Referer': 'https://developers.facebook.com/',
@@ -82,7 +82,13 @@ export default class App {
         'Connection': 'keep-alive'
       },
     }, async (err, res) =>  {
+      console.log(`🛠 LOG: 🚀 --> -------------------------------------------------------------------🛠 LOG: 🚀 -->`);
+      console.log(`🛠 LOG: 🚀 --> ~ file: app.ts ~ line 85 ~ App ~ getData= ~ res`, res);
+      console.log(`🛠 LOG: 🚀 --> -------------------------------------------------------------------🛠 LOG: 🚀 -->`);
       const data = JSON.parse(res.body);
+      console.log(`🛠 LOG: 🚀 --> ---------------------------------------------------------------------🛠 LOG: 🚀 -->`);
+      console.log(`🛠 LOG: 🚀 --> ~ file: app.ts ~ line 89 ~ App ~ getData= ~ data`, data);
+      console.log(`🛠 LOG: 🚀 --> ---------------------------------------------------------------------🛠 LOG: 🚀 -->`);
       const records: any[] = [];
       const next: string = data.paging.next;
       this.next = next;
