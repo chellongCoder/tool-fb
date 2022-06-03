2b5bd3acdfb81c8dd1b07b6e4d7b212065d6b492
Longnn <longnn17.developer@gmail.com>
2022-06-03 19:20:18 +0700

    add identifer

--- src/app.ts
+++ src/app.ts
@@ -57,7 +57,7 @@ export default class App {
   // }
 
   timeout = async () => {
-    return await new Promise(resolve => this.getData(5, resolve));
+    return await new Promise(resolve => this.getData(undefined, resolve));
   }
 
   timeoutInfo = async (ms) => {
@@ -65,9 +65,11 @@ export default class App {
   }
 
   getData = async (limit?: number, resolve?: any, ) => {
-    await axios({
-      method: 'GET',
-      url: this.next,
+    console.log(`🛠 LOG: 🚀 --> -------------------------------------------------------------------------------`);
+    console.log(`🛠 LOG: 🚀 --> ~ file: app.ts ~ line 69 ~ App ~ getData= ~ this.next`, this.next);
+    console.log(`🛠 LOG: 🚀 --> -------------------------------------------------------------------------------`);
+
+     tr.request(this.next, {
       headers: {
         'Accept': '*/*',
         'Content-Type': 'application/x-www-form-urlencoded',
@@ -79,15 +81,13 @@ export default class App {
         'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
         'Connection': 'keep-alive'
       },
-    }).then(async (res) =>  {
-      console.log(`🛠 LOG: 🚀 --> -------------------------------------------------------------------`);
-      console.log(`🛠 LOG: 🚀 --> ~ file: app.ts ~ line 74 ~ App ~ getData= ~ res`, res.data.__fb_trace_id__, res.data.data);
-      console.log(`🛠 LOG: 🚀 --> -------------------------------------------------------------------`);
+    }, async (err, res) =>  {
+      const data = JSON.parse(res.body);
       const records: any[] = [];
-      const next: string = res.data.paging.next;
+      const next: string = data.paging.next;
       this.next = next;
       // for {limit}} thằng lấy ra data page 
-      await Promise.all(res.data.data.slice(0, limit).map(async (element: any, index:number) => {
+      await Promise.all(data.data.slice(0, limit).map(async (element: any, index:number) => {
         await this.timeoutInfo(1000 * index)
         console.log('start request lấy email');
         const info = await axios({
@@ -109,27 +109,14 @@ export default class App {
         console.log(`🛠 LOG: 🚀 --> ~ file: app.ts ~ line 131 ~ App ~ this.app.listen ~ info`, info.data);
         console.log(`🛠 LOG: 🚀 --> -----------------------------------------------------------------------------`);
         records.push({...element, emails: info.data.emails}); // push vào mảng data 
-        fs.appendFileSync('output.json', info.data.emails ? `${info.data.emails}\n` : `\n`);
+        // lưu data vào file 
+        fs.appendFileSync('output.json', info.data.emails ? `${info.data.emails}\n` : ``);
       }))
-      console.log('end', res.data.__fb_trace_id__);
-      resolve(next);
-      // lưu data vào file 
-      // fs.writeFile("output.json", JSON.stringify(res.data.__fb_trace_id__), 'utf8', function (err) {
-      //   if (err) {
-      //       console.log("An error occured while writing JSON Object to File.");
-      //       return console.log(err);
-      //   }
-     
-      //   console.log("JSON file has been saved.");
-      // });
-      // fs.appendFile('output.json', `${JSON.stringify(records)}\n`, function (err) {
-      //   if (err) {
-      //     console.log("An error occured while writing JSON Object to File.");
-      //     return console.log(err);
-      // }
-      //   console.log('Saved!');
-      // });
-    });
+      console.log('end', res.body);
+      resolve(res);
+
+
+    })
   }
   
   run() {
@@ -155,28 +142,6 @@ export default class App {
           await dec();
       }
       foo();
-
-      // tr.request('https://graph.facebook.com/v13.0/pages/search?access_token=EAABsbCS1iHgBAGNU5rxcsa50gO6NvvGZCFMZBqevpuqrddfy3ZBTh2iy9PL7im5sU8dsBc0CZCHYz2Fp89znzvY7116IS7A2p8wcZCEQZBE1K92T6gwZCB1MTCFZBsLXw3ehicssZBbRZCyM6r8ZBblWBZA21GZCGEE1i9qagWfZAHemmqeR0qX76h7mSqe4FA1dHymwgZD&__cppo=1&debug=all&format=json&limit=300&method=get&pretty=0&q=nails&state=%22united%20state%22&suppress_http_code=1&transport=cors&type=country', {
-      //   headers: {
-      //     'Accept': '*/*',
-      //     'Content-Type': 'application/x-www-form-urlencoded',
-      //     'Origin': 'https://developers.facebook.com',
-      //     'Cookie': 'wd=1650x404; usida=eyJ2ZXIiOjEsImlkIjoiQXJjOXYzaXV0MmtzeSIsInRpbWUiOjE2NTMyMDQ1NDZ9; cppo=1; fr=0RTEYl2K91g3DU27Q.AWUML3Ep0mitrHrAgdokduamOz0.Biidm_.ZU.AAA.0.0.Biid3n.AWVRGGhohcM; c_user=100066192924982; xs=5%3Aymwyn3MxZYEPLQ%3A2%3A1653055477%3A-1%3A14128%3A%3AAcXyiBJbeXDJvaLfOxMJnvjqoTixIn3zzLg-BP4bBg; presence=C%7B%22t3%22%3A%5B%5D%2C%22utc3%22%3A1653156441657%2C%22v%22%3A1%7D; sb=fRZdYmmkyLCqUiLG_yqHqUa2; locale=en_US; dpr=2; datr=fRZdYvFPptvdypB4vUSP-Iy_',
-      //     'Host': 'graph.facebook.com',
-      //     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Safari/605.1.15',
-      //     'Referer': 'https://developers.facebook.com/',
-      //     'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
-      //     'Connection': 'keep-alive'
-      //   },
-      // }, function (err, res, body) {
-      // console.log(`🛠 LOG: 🚀 --> ---------------------------------------------------------------------`);
-      // console.log(`🛠 LOG: 🚀 --> ~ file: app.ts ~ line 171 ~ this.app.listen ~ res`, res);
-      // console.log(`🛠 LOG: 🚀 --> ---------------------------------------------------------------------`);
-      //   if (!err && res.statusCode == 200) {
-      //     console.log("Your public (through Tor) IP is: " + JSON.parse(body));
-      //   }
-      // });
-      
 
       
     });
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,3 @@
-import axios from "axios"
 
 var express = require('express')
 var app = express()
@@ -40,74 +39,6 @@ function findExternalIp (request, done) {
   tick()
 }
 
-const getData = async (limit: number, resolve?: any, ) => {
-  tr.request({
-    method: 'GET',
-    url: 'https://graph.facebook.com/v13.0/pages/search?access_token=EAABsbCS1iHgBAGNU5rxcsa50gO6NvvGZCFMZBqevpuqrddfy3ZBTh2iy9PL7im5sU8dsBc0CZCHYz2Fp89znzvY7116IS7A2p8wcZCEQZBE1K92T6gwZCB1MTCFZBsLXw3ehicssZBbRZCyM6r8ZBblWBZA21GZCGEE1i9qagWfZAHemmqeR0qX76h7mSqe4FA1dHymwgZD&__cppo=1&debug=all&format=json&limit=300&method=get&pretty=0&q=nails&state=%22united%20state%22&suppress_http_code=1&transport=cors&type=country',
-    headers: {
-      'Accept': '*/*',
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Origin': 'https://developers.facebook.com',
-      'Cookie': 'wd=1650x404; usida=eyJ2ZXIiOjEsImlkIjoiQXJjOXYzaXV0MmtzeSIsInRpbWUiOjE2NTMyMDQ1NDZ9; cppo=1; fr=0RTEYl2K91g3DU27Q.AWUML3Ep0mitrHrAgdokduamOz0.Biidm_.ZU.AAA.0.0.Biid3n.AWVRGGhohcM; c_user=100066192924982; xs=5%3Aymwyn3MxZYEPLQ%3A2%3A1653055477%3A-1%3A14128%3A%3AAcXyiBJbeXDJvaLfOxMJnvjqoTixIn3zzLg-BP4bBg; presence=C%7B%22t3%22%3A%5B%5D%2C%22utc3%22%3A1653156441657%2C%22v%22%3A1%7D; sb=fRZdYmmkyLCqUiLG_yqHqUa2; locale=en_US; dpr=2; datr=fRZdYvFPptvdypB4vUSP-Iy_',
-      'Host': 'graph.facebook.com',
-      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Safari/605.1.15',
-      'Referer': 'https://developers.facebook.com/',
-      'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
-      'Connection': 'keep-alive'
-    },
-
-  }, ).then(async (res) =>  {
-    console.log(`🛠 LOG: 🚀 --> -------------------------------------------------------------------`);
-    console.log(`🛠 LOG: 🚀 --> ~ file: app.ts ~ line 74 ~ App ~ getData= ~ res`, res.data.__fb_trace_id__, res.data.data);
-    console.log(`🛠 LOG: 🚀 --> -------------------------------------------------------------------`);
-    const records: any[] = [];
-    const next: string = res.data.paging.next;
-
-    // for {limit}} thằng lấy ra data page 
-    // await Promise.all(res.data.data.map(async (element: any, index:number) => {
-    //   await this.timeoutInfo(1000 * index)
-    //   console.log('start request lấy email');
-    //   const info = await axios({
-    //     method: 'GET',
-    //     url: `https://graph.facebook.com/v13.0/${element.id}?access_token=EAABsbCS1iHgBAGNU5rxcsa50gO6NvvGZCFMZBqevpuqrddfy3ZBTh2iy9PL7im5sU8dsBc0CZCHYz2Fp89znzvY7116IS7A2p8wcZCEQZBE1K92T6gwZCB1MTCFZBsLXw3ehicssZBbRZCyM6r8ZBblWBZA21GZCGEE1i9qagWfZAHemmqeR0qX76h7mSqe4FA1dHymwgZD&__cppo=1&debug=all&fields=emails&format=json&method=get&pretty=0&suppress_http_code=1&transport=cors`,
-    //     headers: {
-    //       'Accept': '*/*',
-    //       'Content-Type': 'application/x-www-form-urlencoded',
-    //       'Origin': 'https://developers.facebook.com',
-    //       'Cookie': 'wd=1650x404; usida=eyJ2ZXIiOjEsImlkIjoiQXJjOXYzaXV0MmtzeSIsInRpbWUiOjE2NTMyMDQ1NDZ9; cppo=1; fr=0RTEYl2K91g3DU27Q.AWUML3Ep0mitrHrAgdokduamOz0.Biidm_.ZU.AAA.0.0.Biid3n.AWVRGGhohcM; c_user=100066192924982; xs=5%3Aymwyn3MxZYEPLQ%3A2%3A1653055477%3A-1%3A14128%3A%3AAcXyiBJbeXDJvaLfOxMJnvjqoTixIn3zzLg-BP4bBg; presence=C%7B%22t3%22%3A%5B%5D%2C%22utc3%22%3A1653156441657%2C%22v%22%3A1%7D; sb=fRZdYmmkyLCqUiLG_yqHqUa2; locale=en_US; dpr=2; datr=fRZdYvFPptvdypB4vUSP-Iy_',
-    //       'Host': 'graph.facebook.com',
-    //       'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Safari/605.1.15',
-    //       'Referer': 'https://developers.facebook.com/',
-    //       'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
-    //       'Connection': 'keep-alive'
-    //     }
-    //   });
-    //   console.log(`🛠 LOG: 🚀 --> -----------------------------------------------------------------------------`);
-    //   console.log(`🛠 LOG: 🚀 --> ~ file: app.ts ~ line 131 ~ App ~ this.app.listen ~ info`, info.data);
-    //   console.log(`🛠 LOG: 🚀 --> -----------------------------------------------------------------------------`);
-    //   records.push({...element, emails: info.data.emails}); // push vào mảng data 
-    //   fs.appendFileSync('output.json', info.data.emails ? `${info.data.emails}\n` : `\n`);
-    // }))
-    // console.log('end', res.data.__fb_trace_id__);
-    resolve(next);
-    // lưu data vào file 
-    // fs.writeFile("output.json", JSON.stringify(res.data.__fb_trace_id__), 'utf8', function (err) {
-    //   if (err) {
-    //       console.log("An error occured while writing JSON Object to File.");
-    //       return console.log(err);
-    //   }
-   
-    //   console.log("JSON file has been saved.");
-    // });
-    // fs.appendFile('output.json', `${JSON.stringify(records)}\n`, function (err) {
-    //   if (err) {
-    //     console.log("An error occured while writing JSON Object to File.");
-    //     return console.log(err);
-    // }
-    //   console.log('Saved!');
-    // });
-  });
-}
 
 var limiter = require('express-rate-limit')({
   windowMs: 1000 * 60 * 15, // 15 min
@@ -128,6 +59,38 @@ app.use(function (req, res, next) {
 
 app.get('/api/myip', function (req, res) {
   res.send(req.headers['x-forwarded-for'] || req.ip)
+})
+
+app.get('/api/identify', async function (req, res) {
+  const result = await fetch("https://www.facebook.com/ajax/login/help/identify.php?ctx=recover", {
+    "headers": {
+      "accept": "*/*",
+      "accept-language": "en-GB,en;q=0.9",
+      "cache-control": "no-cache",
+      "content-type": "application/x-www-form-urlencoded",
+      "pragma": "no-cache",
+      "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"102\", \"Google Chrome\";v=\"102\"",
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": "\"macOS\"",
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-origin",
+      "x-fb-lsd": "AVquKqj3HjI",
+      "cookie": "fr=0qakZVo816TYN64W4..BimcXa.NI.AAA.0.0.BimcXa.AWVOSIzSc7w; sb=2sWZYlnfqJadqu-ZoEwQc6Np; dpr=2; datr=2sWZYq0LSg9E6UIkiAFOnOLO; wd=807x908; sfiu=AYh5R5hxPkVidmBpclKHL2LcI_hyaZK4mBOXUQBusutPwhpQMLno6qaN1VveDDO9qLTesh6hnMsqNY3oNClWq1IVAVJ0-7tpJ9KH-4ZLnN48nfCv15QLtobCfSYEDLw7seiKUnm7BldVK8kyTbcAJ1td1RGDTuXW6NWZ7N-cS8DZQLaGwGuRBNCw2wXgbpav1bO2Jb_C-FD5rW_V9Gmhe3QR",
+      "Referer": "https://www.facebook.com/login/identify/?ctx=recover&ars=facebook_login&from_login_screen=0",
+      "Referrer-Policy": "origin-when-cross-origin"
+    },
+    "body": "jazoest=2977&lsd=AVquKqj3HjI&email=service%40adorebeauty.com.a&did_submit=1&__user=0&__a=1&__dyn=7xeUmBwjbg7ebwKBWo5O12wAxu13wqovzEdEc8uxa0z8S2S4o1j8hwem0nCq1ewcG0KEswaq0yE7i0n2US1vw9W1PwBgao884y0Mo5W3S1lwlEjxG0y8jwVw9O1iwKwHw8W1uwa-7U1bobodEGdw46wbS1bwzw&__csr=&__req=c&__hs=19146.BP%3ADEFAULT.2.0.0.0.&dpr=2&__ccg=UNKNOWN&__rev=1005626680&__s=bvcwwo%3Aiidmvf%3A1eswrk&__hsi=7104937690027916183-0&__comet_req=0&__spin_r=1005626680&__spin_b=trunk&__spin_t=1654247215",
+    "method": "POST"
+  });
+  const data = await (await result.text()).replace("for (;;);", "")
+  console.log(`🛠 LOG: 🚀 --> -----------------------------------------------------------🛠 LOG: 🚀 -->`);
+  console.log(`🛠 LOG: 🚀 --> ~ file: server.ts ~ line 87 ~ result`, data, JSON.parse(data));
+  console.log(`🛠 LOG: 🚀 --> -----------------------------------------------------------🛠 LOG: 🚀 -->`);
+
+  res.send(JSON.parse(data))
+  
+
 })
 
 app.get('/api/getdata', async function (req, response) {
